5d6f056415803662ca4255de7a90e963d565c48d
Leo <161986680+leovaldezhq@users.noreply.github.com>
2025-08-07 16:49:05 -0300

    Add files via upload

---
+++ Desafios IV - Arrays/app.js
@@ -0,0 +1,17 @@
+//desafio 1
+let listaGenerica = [];
+
+//desafio 2
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+//desafio 3
+linguagensDeProgramacao.push ('Java', 'Ruby', 'GoLang');
+
+//desafio 4
+let yasLikes = ['Luna', 'Bolo', 'Sorvete'];
+
+//desafio 5
+let jogadoras = ['Marta', 'Tarciane', 'Jhonson'];
+
+//desafio 6
+let times = ['Corinthians', 'St. Pauli', 'Barcelona']
