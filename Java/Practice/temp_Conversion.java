46bbdd065d582fa3d793e0d812db270c25b86989
Leo <161986680+leovaldezhq@users.noreply.github.com>
2026-02-02 18:48:44 -0300

    Fixed indentation issues and changed class name

--- Java/Practice/temp_Conversion.java
+++ Java/Practice/temp_Conversion.java
@@ -1,13 +1,16 @@
-public class MyClass {
-public static void main(String[] args) {
-      double temperatureInCelsius = 27;
-      double temperatureInFahrenheit = (temperatureInCelsius * 1.8) + 32;
+public class TemperatureChallenge {
 
-      String message = String.format("A %.1f Celsius temperature is equivalent to %.1f in Fahrenheit", temperatureInCelsius, temperatureInFahrenheit);
-      
-      System.out.println(message);
-      
-      int fahrenheitInteger = (int) temperatureInFahrenheit;
-      System.out.println("The temperature in Fahrenheit with no decimal places is: " + fahrenheitInteger);
-  }
+    public static void main(String[] args) {
+        double temperatureInCelsius = 27;
+        double temperatureInFahrenheit = (temperatureInCelsius * 1.8) + 32;
+
+        String message = String.format("A %.1f Celsius temperature is equivalent to %.1f in Fahrenheit", temperatureInCelsius, temperatureInFahrenheit);
+
+        System.out.println(message);
+
+        int fahrenheitInteger = (int) temperatureInFahrenheit;
+        System.out.println("The temperature in Fahrenheit with no decimal places is: " + fahrenheitInteger);
+
+    }
+
 }
