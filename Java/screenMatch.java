d5bff1683178274c65c026dc9cb588bb1435d311
Leo <161986680+leovaldezhq@users.noreply.github.com>
2026-02-02 18:43:57 -0300

    Update main method parameter name from 'args[]' to 'args'

--- Java/screenMatch.java
+++ Java/screenMatch.java
@@ -1,5 +1,5 @@
 public class MyClass {
-  public static void main(String args[]) {
+  public static void main(String[] args) {
       System.out.println("This is Screen Match");
       System.out.println("Film: V for Vendetta");
       
